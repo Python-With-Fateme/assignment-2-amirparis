8f5730d448386c84db3d5eca572565bc81c6c1dc
unknown <you@example.com>
2022-08-31 13:11:01 +0430

    first

---
+++ as2-1.py
@@ -0,0 +1,15 @@
+a=int(input(' enter first grade : '))
+b=int(input(' enter second grade : '))
+c=int(input(' enter third grade : '))
+d=int(input(' enter fourth grade : '))
+e=int(input(' enter fifth grade : '))
+m=(a+b+c+d+e)/5
+if m>17:
+    print=(m)
+    print(' student is the best ')
+if m<17 and m>12:
+    print(m)
+    print(' student is normal ')
+else :
+    print(m)
+    print(' student is failed ')
---
+++ as2-3.py
@@ -0,0 +1,16 @@
+
+a=float(input(' enter your first number : '))
+b=float(input(' enter your second number : '))
+c=float(input(' enter your third number : '))
+d=float(input(' enter your fourth number : '))
+e=float(input(' enter your fifth number : '))
+if a>b and a>c and a>d and a>e : 
+    print(a)
+if b>a and b>c and b>d and b>e :
+    print(b)
+if c>a and c>b and c>d and c>e :
+    print(c)
+if d>a and d>b and d>c and d>e :
+    print(d)
+if e>a and e>b and e>c and e>d :
+    print(e)
